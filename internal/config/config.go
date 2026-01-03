697043604ecca84fe813967cf515dc4a461f9c9f
Grimm <fvckgrimm@protonmail.ch>
2026-01-03 12:51:41 -0800

    mainurl
    i should setup a local dev env for easier switching

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,6 +5,6 @@ var Version = "dev"
 const (
 	KeyringService = "entropy-systems"
 	UserAccount    = "active-signer"
-	BaseURL        = "http://localhost:8787"
-	//BaseURL = "https://api.x402systems.online"
+	//BaseURL        = "http://localhost:8787"
+	BaseURL = "https://api.x402systems.online"
 )
